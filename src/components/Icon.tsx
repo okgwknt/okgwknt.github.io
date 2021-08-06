5d9d194132c1dc840cf1b6bcec66000a63807143
okgwknt <okukyan@gmail.com>
2021-08-07 01:18:13 +0900

    fix

--- src/components/Icon.tsx
+++ src/components/Icon.tsx
@@ -1,18 +1,15 @@
 import { VFC } from "react";
 
+import Image from "next/image";
+
 type Props = {
   link: string;
   name: string;
   imageName: string;
 };
 
-// fontowesome 導入
-
 export const Icon: VFC<Props> = ({ link, name, imageName }) => (
-  // className="transform hover:scale-110 motion-reduce:transform-none"
-
   <a href={link} target="_blank" rel="noreferrer">
-    {/* {name} */}
     <img
       src={imageName}
       alt={name}
